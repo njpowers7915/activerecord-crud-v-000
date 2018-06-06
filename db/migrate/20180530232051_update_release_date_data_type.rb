60ee4fbfd54a5394609c94d10f2ad6d5affae2a4
Nicholas Powers <njpowers7915@gmail.com>
2018-06-06 01:53:31 +0000

    automatically backed up by learn

--- db/migrate/20180530232051_update_release_date_data_type.rb
+++
@@ -1,6 +0,0 @@
-class UpdateReleaseDateDataType < ActiveRecord::Migration
-  def change
-    remove_column :movies, :release_date, :string
-    add_column :movies, :release_date, :integer
-  end
-end
